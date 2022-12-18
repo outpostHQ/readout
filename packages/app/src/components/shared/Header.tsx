2f7beb51d24cd9376be74cb20e1b09ffc62f03c3
taranbeer <staranbeer@gmail.com>
2022-12-18 17:51:20 +0530

    created the layout

---
+++ packages/app/src/components/shared/Header.tsx
@@ -0,0 +1,12 @@
+import { Block } from '@cube-dev/ui-kit';
+import Container from '../utils/utils';
+
+function Header() {
+  return (
+    <Block styles={{ border: '1px solid #3E3E3E' }} padding="1.5rem 0">
+      <Container>header</Container>
+    </Block>
+  );
+}
+
+export default Header;
