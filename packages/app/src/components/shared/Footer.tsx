5d317538897b8e7e0890dd3ce0bc524f2432c479
taranbeer <staranbeer@gmail.com>
2022-12-19 12:39:45 +0530

    integrated footer with the configContext

--- packages/app/src/components/shared/Footer.tsx
+++ packages/app/src/components/shared/Footer.tsx
@@ -1,10 +1,31 @@
-import { Block } from '@cube-dev/ui-kit';
+import { Block, Flex } from '@cube-dev/ui-kit';
+import { useContext, useState } from 'react';
+import ConfigContext from '../../context/ConfigContext';
 import Container from '../utils/utils';
-
+import { RiGithubLine, RiDiscordLine, RiTwitterLine } from 'react-icons/ri';
 function Footer() {
+  const [{ footerSocials }] = useContext(ConfigContext);
   return (
-    <Block styles={{ borderTop: '1px solid #3E3E3E' }} padding="1.5rem 0">
-      <Container>header</Container>
+    <Block styles={{ borderTop: '1px solid #3E3E3E', color: '#ffffff' }} padding="1.5rem 0">
+      <Container>
+        <Flex gap="1.5rem" justifyContent="flex-end">
+          {footerSocials.discord && (
+            <a href={footerSocials.discord}>
+              <RiDiscordLine size={28} />
+            </a>
+          )}
+          {footerSocials.twitter && (
+            <a href={footerSocials.twitter}>
+              <RiTwitterLine size={28} />
+            </a>
+          )}
+          {footerSocials.github && (
+            <a href={footerSocials.github}>
+              <RiGithubLine size={28} />
+            </a>
+          )}
+        </Flex>
+      </Container>
     </Block>
   );
 }
