9da3a764ca31726c27982dc5eebbedb5732c7ddc
taranbeer <staranbeer@gmail.com>
2022-12-22 22:28:05 +0530

    testing without codehike

--- packages/app/src/lib/getMDXContent.ts
+++ packages/app/src/lib/getMDXContent.ts
@@ -13,25 +13,28 @@ async function getMdxContent(
 ) {
   const octokit = new Octokit({ auth: process.env.GITHUB_PAT });
   let route = `/${path}/${page}.mdx`;
-  const { data }: any = await octokit.rest.repos.getContent({
-    owner: owner,
-    repo: repoName,
-    path: route,
-  });
-  let mdx;
+  try {
+    const { data }: any = await octokit.rest.repos.getContent({
+      owner: owner,
+      repo: repoName,
+      path: route,
+    });
+    let mdx;
+    if (Array.isArray(data)) {
+      mdx = Buffer.from(data[0].content!, 'base64').toString('ascii');
+    } else {
+      mdx = Buffer.from(data?.content, 'base64').toString('ascii');
+    }
 
-  if (Array.isArray(data)) {
-    mdx = Buffer.from(data[0].content!, 'base64').toString('ascii');
-  } else {
-    mdx = Buffer.from(data?.content, 'base64').toString('ascii');
+    let pageContent = await bundle(mdx, {
+      rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+      remarkPlugins: [],
+      headerDepth: 3,
+    });
+    return pageContent;
+  } catch (err) {
+    return err;
   }
-
-  let pageContent = await bundle(mdx, {
-    rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
-    remarkPlugins: [[remarkCodeHike, { showCopyButton: true, theme }]],
-    headerDepth: 3,
-  });
-  return pageContent;
 }
 
 export { getMdxContent };
