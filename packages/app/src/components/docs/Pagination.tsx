2994ec40da187335c718d7f0121f400d49a044f6
taranbeer <staranbeer@gmail.com>
2022-12-22 17:48:06 +0530

    created the pagination component

--- packages/app/src/components/docs/Pagination.tsx
+++ packages/app/src/components/docs/Pagination.tsx
@@ -1,10 +1,16 @@
-import { Flex } from '@cube-dev/ui-kit';
-
+import { Block, Flex } from '@cube-dev/ui-kit';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 function Pagination() {
   return (
     <Flex justifyContent="space-between">
-      <Flex></Flex>
-      <Flex></Flex>
+      <Flex alignItems="flex-end" gap="1rem">
+        <HiChevronLeft size={20} />
+        <Block>Overview</Block>
+      </Flex>
+      <Flex alignItems="flex-end" gap="1rem">
+        <Block>API</Block>
+        <HiChevronRight size={20} />
+      </Flex>
     </Flex>
   );
 }
