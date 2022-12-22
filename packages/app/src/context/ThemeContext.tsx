37e44c3c00a1d06fc38e792969c2573485fa2b5c
taranbeer <staranbeer@gmail.com>
2022-12-22 10:03:40 +0530

    added codehike

--- packages/app/src/context/ThemeContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, ReactNode } from 'react';
-
-export const ThemeContext = createContext({});
-
-function ThemeProvider({ children }: { children: ReactNode }) {
-  return <ThemeContext.Provider value={{}}>{children}</ThemeContext.Provider>;
-}
-
-export default ThemeProvider;
--- packages/app/src/pages/index.tsx
+++ packages/app/src/pages/index.tsx
@@ -1,11 +1,7 @@
 import { Flex } from '@cube-dev/ui-kit';
-import { useContext } from 'react';
 import AddGithubRepo from '../components/AddGithubRepo';
-import ConfigContext from '../context/ConfigContext';
 
 function Home() {
-  const [values, setValues] = useContext(ConfigContext);
-
   return (
     <Flex
       height="100vh"
