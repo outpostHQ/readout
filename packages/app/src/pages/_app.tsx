59cc4da1f53b0ef841c2a6bb96b61416ec13e8e7
taranbeer <staranbeer@gmail.com>
2022-12-22 18:54:07 +0530

    fixed the layout

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -3,17 +3,24 @@ import type { AppProps } from 'next/app';
 import { BreakpointsProvider, Root, SSRProvider } from '@cube-dev/ui-kit';
 import Layout from '../components/shared/Layouts/Layout';
 import '@code-hike/mdx/dist/index.css';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
-    <Root bodyStyles={{ 'background-color': '#121312' }}>
-      <SSRProvider>
+    <SSRProvider>
+      <Root bodyStyles={{ 'background-color': '#121312' }}>
         <BreakpointsProvider value={[1140, 900, 500]}>
-          <Layout>
+          {router.asPath !== '/' ? (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          ) : (
             <Component {...pageProps} />
-          </Layout>
+          )}
         </BreakpointsProvider>
-      </SSRProvider>
-    </Root>
+      </Root>
+    </SSRProvider>
   );
 }
