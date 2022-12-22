dab8db7cd4a41041126a9493ca4fba00c1e35df5
taranbeer <staranbeer@gmail.com>
2022-12-22 17:17:59 +0530

    minor changes

---
+++ packages/app/src/components/docs/Pagination.tsx
@@ -0,0 +1,12 @@
+import { Flex } from '@cube-dev/ui-kit';
+
+function Pagination() {
+  return (
+    <Flex justifyContent="space-between">
+      <Flex></Flex>
+      <Flex></Flex>
+    </Flex>
+  );
+}
+
+export default Pagination;
